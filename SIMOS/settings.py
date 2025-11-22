7a205ef709da9c3556a7bb6661f31d4ce7b6b168
Jijiluzana225 <jiji.luzana@gmail.com>
2025-11-22 14:00:19 +0800

    adsf

--- SIMOS/settings.py
+++ SIMOS/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app',
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
